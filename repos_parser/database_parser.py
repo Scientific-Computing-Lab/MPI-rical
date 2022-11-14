8623bc9178dab68a9ab3c1a8ccbf0a9a2d15476b
NadavSc <nadavsch19ad@gmail.com>
2022-11-14 13:52:04 +0200

    database.json

--- repos_parser/database_parser.py
+++ repos_parser/database_parser.py
@@ -4,12 +4,12 @@ import json
 
 from repos_parser import Database
 
-database = Database()
-# funcs_counter = database.total_mpi_functions()
-# keys, values = zip(*dict(sorted(funcs_counter.items(), key=lambda item: item[1], reverse=True)).items())
-#
-# fig, ax = plt.subplots(1, 1)
-# ax.set_title('Functions Distribution')
-# ax.bar(keys[:30], values[:30], color='g')
-# plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
-# plt.show()
+database = Database('database.json')
+funcs_counter = database.total_mpi_functions()
+keys, values = zip(*dict(sorted(funcs_counter.items(), key=lambda item: item[1], reverse=True)).items())
+
+fig, ax = plt.subplots(1, 1)
+ax.set_title('Functions Distribution')
+ax.bar(keys[:30], values[:30], color='g')
+plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='right')
+plt.show()
