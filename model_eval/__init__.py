56542c7173eec6b047a758a0f0b3b3e22ee83458
NadavSc <nadavsch19ad@gmail.com>
2023-05-16 01:26:39 +0300

    1. added F1 with locations by lines 2. past F1 calculation update

--- model_eval/__init__.py
+++ model_eval/__init__.py
@@ -1,8 +1,28 @@
 import re
 
+common_core_funcs = ['mpi _init',
+                     'mp i_ finalize',
+                     'mp i_ comm_ size',
+                     'mp i_ comm_ rank',
+                     'mp i_ recv',
+                     'mp i_ send',
+                     'mpi _b cast',
+                     'mpi _reduce']
+
+
+def common_core_only(func_matches):
+    core_matches = {}
+    for func_name, matches in func_matches.items():
+        if func_name in common_core_funcs or 'reduce' in func_name:
+            core_matches[func_name] = matches
+    return core_matches
+
 
 def prefix_function(func):
-    return re.search(r'mp[\s]?i[^;(]*', func).group().strip()
+    prefix = re.search(r'mp[\s]?i[^;(]*', func)
+    if prefix:
+        return prefix.group().strip()
+    return ''
 
 
 def remove_mpi_funcs(code, matches):
@@ -24,6 +44,57 @@ def matcher(functions):
     return funcs
 
 
+def is_mpi_func_include(tokens, mpi_funcs):
+    for token in tokens:
+        if token in mpi_funcs:
+            return True
+    return False
+
+
+def get_near_tokens(token_idx, tokens):
+    tokens_near = []
+    for j in range(-2, 3, 1):
+        if token_idx + j > 0 and token_idx + j < len(tokens):
+            tokens_near.append(tokens[token_idx + j])
+    return tokens_near
+
+
+def calc_tp_fp_fn(reference, candidate, common_core=False):
+    tp = {}
+    fp = {}
+    fn = {}
+    ref_functions = list(re.finditer(r'mp[\s]?i[\s\w_]*[(][^;]*[)][\s]?;', reference))
+    cand_functions = list(re.finditer(r'mp[\s]?i[\s\w_]*[(][^;]*[)][\s]?;', candidate))
+    pre_ref_functions = [prefix_function(func.group()) for func in ref_functions]
+    pre_cand_functions = [prefix_function(func.group()) for func in cand_functions]
+    reference = reference.split(';')
+    candidate = candidate.split(';')
+    reference = [prefix_function(token) for token in reference]
+    candidate = [prefix_function(token) for token in candidate]
+    for token_idx, token_ref in enumerate(reference):
+        if token_ref in pre_ref_functions:
+            if common_core and token_ref not in common_core_funcs:
+                continue
+            tokens_cand = get_near_tokens(token_idx, candidate)
+            if token_ref in tokens_cand:
+                tp[token_ref] = (tp[token_ref] if token_ref in tp else 0) + 1
+            elif is_mpi_func_include(tokens_cand, pre_cand_functions):
+                fp[token_ref] = (fp[token_ref] if token_ref in fp else 0) + 1
+            else:
+                fn[token_ref] = (fn[token_ref] if token_ref in fn else 0) + 1
+
+    for token_idx, token_cand in enumerate(candidate):
+        if token_cand in pre_cand_functions:
+            if common_core and token_cand not in common_core_funcs:
+                continue
+            tokens_ref = get_near_tokens(token_idx, reference)
+            if not is_mpi_func_include(tokens_ref, pre_ref_functions):
+                print(token_cand)
+                print(tokens_ref)
+                fp[token_cand] = (fp[token_cand] if token_cand in fp else 0) + 1
+    return sum(tp.values()), sum(fp.values()), sum(fn.values())
+
+
 def calc_tp_fp(ref_matches, cand_matches):
     tp = {}
     fp = {}
@@ -36,7 +107,7 @@ def calc_tp_fp(ref_matches, cand_matches):
             p += 1
             if func in cand_matches:
                 for cand_match in cand_matches[func]:
-                    if abs(cand_match.span()[0]-ref_match_start) < 80:
+                    if abs(cand_match.span()[0]-ref_match_start) < 100:
                         matches += 1
                         tp[func] = (tp[func] if func in tp else 0) + 1
             else:
@@ -60,37 +131,46 @@ def calc_tn_fn(fn, reference, candidate, ref_matches, cand_matches, verbose=0):
     reference = reference.split()
     candidate = candidate.split()
     tn = 0
-    fn += abs(len(reference) - len(candidate))
+    # fn += abs(len(reference) - len(candidate))
     n = len(reference) if len(reference) > len(candidate) else len(candidate)
     for ref_token, cand_token in zip(reference, candidate):
         if ref_token == cand_token:
             tn += 1
         elif verbose > 0:
             print(f'ref token - {ref_token} | cand token {cand_token}')
-            fn += 1
+            # fn += 1
     return tn, fn, n
 
 
 def metrics_calc(tp, tn, fn, fp):
-    tpr = tp / (tp + fn)
-    tnr = tn / (tn + fp)
-    fnr = fn / (fn + tp)
-    fpr = fp / (fp + tn)
+    # tpr = tp / (tp + fn)
+    # tnr = tn / (tn + fp)
+    # fnr = fn / (fn + tp)
+    # fpr = fp / (fp + tn)
+    tpr = 0
+    tnr = 0
+    fnr = 0
+    fpr = 0
     precision = tp / (tp + fp)
     recall = tp / (tp + fn)
     f1 = 2 * (precision * recall / (precision + recall))
     return tpr, tnr, fnr, fpr, precision, recall, f1
 
 
-def conf_matrix(reference, candidate, metrics=False):
-    ref_functions = list(re.finditer(r'mp[\s]?i[^;)=]*[(][^);]*[)][\s]?;', reference))
-    cand_functions = list(re.finditer(r'mp[\s]?i[^;)=]*[(][^);]*[)][\s]?;', candidate))
-    ref_matches = matcher(ref_functions)
-    cand_matches = matcher(cand_functions)
-    tp, fp, fn, p = calc_tp_fp(ref_matches, cand_matches)
-    tn, fn, n = calc_tn_fn(fn, reference, candidate, ref_matches, cand_matches)
-    if metrics:
-        tpr, tnr, fnr, fpr, precision, recall, f1 = metrics_calc(tp=tp, tn=tn, fn=fn, fp=fp)
+def conf_matrix(reference, candidate, metrics=False, common_core=False):
+    tp, fp, fn = calc_tp_fp_fn(reference, candidate, common_core)
+    # ref_functions = list(re.finditer(r'mp[\s]?i[\s\w_]*[(][^;]*[)][\s]?;', reference))
+    # cand_functions = list(re.finditer(r'mp[\s]?i[\s\w_]*[(][^;]*[)][\s]?;', candidate))
+    # ref_matches = common_core_only(matcher(ref_functions)) if common_core else matcher(ref_functions)
+    # cand_matches = common_core_only(matcher(cand_functions)) if common_core else matcher(cand_functions)
+    #
+    # tp, fp, fn, p = calc_tp_fp(ref_matches, cand_matches)
+    # tn, fn, n = calc_tn_fn(fn, reference, candidate, ref_matches, cand_matches)
+    # if metrics:
+    #     tpr, tnr, fnr, fpr, precision, recall, f1 = metrics_calc(tp=tp, tn=tn, fn=fn, fp=fp)
+    tn = 0
+    p = 0
+    n = 0
     return tp, fp, tn, fn, p, n
 
 
